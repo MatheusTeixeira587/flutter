b09de1f0df007cb818eb39a511724a9465912572
matheus teixeira câmara <matheus.t98@gmail.com>
2019-11-01 12:51:49 -0300

    hello on screen

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
-}
+void main(List<String> args) => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) => MaterialApp(home: Text("hello!"));
+  int currentIndex = 0;
+  final questions = [
+    "What\'s your favorite color?",
+    "What\'s your favorite animal?",
+  ];
+
+  void answerQuestion() {
+    print('answer chosen');
+    currentIndex++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("My quiz!"),
+      ),
+      body: Column(
+        children: [
+          Text(questions.elementAt(currentIndex > questions.length ? 0 : currentIndex)),
+          RaisedButton(
+            child: Text("Answer 1 for question $currentIndex"),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text("Answer 2 for question $currentIndex"),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text("Answer 3 for question $currentIndex"),
+            onPressed: answerQuestion,
+          ),
+        ],
+      ),
+    ));
+  }
 }
